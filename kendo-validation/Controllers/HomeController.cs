2e2783eabf5241313c8c87b9f151e0ed6c34949a
George Krystev <ggkrustev@yahoo.com>
2012-12-15 11:58:11 +0200

    Demo for validation of comple Kendo widgets

---
+++ kendo-validation/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoInputs_Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult KendoValidate()
+        {
+            return View();
+        }
+
+        public ActionResult KendoMvcValidate()
+        {
+            return View();
+        }
+
+        public ActionResult jQuery_KendoMvcValidate()
+        {
+            return View();
+        }
+    }
+}
