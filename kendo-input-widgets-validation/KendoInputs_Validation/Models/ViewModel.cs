b1504c35fb8943fb2e4e78e863621de32840fefa
George Krystev <ggkrustev@yahoo.com>
2013-03-20 12:21:55 +0200

    Code library, which shows how to validate different widgets. Project validates most common kendo input widgets using Kendo Validator and jQuery validate framework

---
+++ kendo-input-widgets-validation/KendoInputs_Validation/Models/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KendoInputs_Validation.Models
+{
+    public class ViewModel
+    {
+        [Required]
+        public double Number { get; set; }
+
+        [Required]
+        public string ComboBox { get; set; }
+
+        [Required]
+        public string DropDownList { get; set; }
+
+        [Required]
+        public DateTime DatePicker { get; set; }
+    }
+}
